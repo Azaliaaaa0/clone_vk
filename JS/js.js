d677f0b706719ab3629c984ac3538e408ad5d5e7
Livyliv <islamgulovaazalia7@gmail.com>
2025-10-29 23:31:07 +0400

    Описание изменений

--- JS/js.js
+++ JS/js.js
@@ -1,34 +1,11 @@
-// блок с фотками, музыкой и тд
-const links = document.querySelectorAll('.tab-btn');
-const contents = document.querySelectorAll('.tab-content');
+let contentbtn = document.querySelectorAll('.tab_btn');
+let content = document.querySelectorAll('.tab_content');
 
-links.forEach(link => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    const tabId = link.getAttribute('data-tab');
+contentbtn.forEach(tab_btn => {
+  tab_btn.addEventListener('click', () => {
+  
+})
 
-    links.forEach(l => l.classList.remove('active'));
-    contents.forEach(c => c.style.display = 'none');
-
-    link.classList.add('active');
-    document.getElementById(tabId).style.display = 'block';
-  });
-});
-
-
-// блок с постами в профиле
-const post = document.querySelectorAll('.tabs-btn');
-const postContent = document.querySelectorAll('.tabs-content');
-post.forEach(button => {
-  button.addEventListener('click', () => {
-    if (button.classList.contains('search-btns')) return;
-    const tabId = button.getAttribute('data-tab');
-
-    post.forEach(btn => btn.classList.remove('active'));
-    postContent.forEach(content => content.style.display = 'none');
-
-    button.classList.add('active');
-    document.getElementById(tabId).style.display = 'block';
-  });
-});
+})
+content.innerHTML = 
 
