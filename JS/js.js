c1b5d3e45b6334a2ec6d1f14ced4243c672e138f
Livyliv <islamgulovaazalia7@gmail.com>
2025-11-02 20:47:36 +0400

    Описание изменений

--- JS/js.js
+++ JS/js.js
@@ -1,11 +1,14 @@
-let contentbtn = document.querySelectorAll('.tab_btn');
-let content = document.querySelectorAll('.tab_content');
+let contentbtn = document.querySelectorAll('.tab_btn')
+let content = document.querySelectorAll('.tab_content')
 
-contentbtn.forEach(tab_btn => {
+
+contentbtn.forEach((tab_btn, i) => {
   tab_btn.addEventListener('click', () => {
-  
-})
+    contentbtn.forEach(b=>b.classList.remove('active'));
+    content.forEach(c=>c.style.display = 'none');
 
+    tab_btn.classList.add('active');
+    content[i].style.display = '';
+  })
 })
-content.innerHTML = 
 
